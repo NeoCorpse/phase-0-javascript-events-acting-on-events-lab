e134d54cd80e22ffda9e59d86f252acfc03398a3
Ian <neocorpse@proton.me>
2025-02-22 22:08:27 +0300

    Create functions for moving dodger depending on key pressed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+const dodger = document.querySelector("#dodger");
+dodger.style.backgroundColor = "#FF69B4";
+const game = document.querySelector("#game");
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") moveDodgerLeft();
+  else if (e.key === "ArrowRight") moveDodgerRight();
+  else if (e.key === "ArrowUp") {
+    let bottom = dodger.style.bottom.replace("px", "");
+    if (bottom >= 380) return;
+    dodger.style.bottom = `${+bottom + 5}px`;
+  } else if (e.key === "ArrowDown") {
+    let bottom = dodger.style.bottom.replace("px", "");
+    if (bottom <= 0) return;
+    dodger.style.bottom = `${+bottom - 5}px`;
+  }
+});
+
+function moveDodgerLeft() {
+  let left = dodger.style.left.replace("px", "");
+  if (left <= 0) return;
+  dodger.style.left = `${+left - 5}px`;
+}
+
+function moveDodgerRight() {
+  let left = dodger.style.left.replace("px", "");
+  if (left >= 360) return;
+  dodger.style.left = `${+left + 5}px`;
+}
